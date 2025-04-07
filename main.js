645815ba8c869137fa096d032d36bd39fdd7fd1d
mariricken <146238773+mariricken@users.noreply.github.com>
2025-04-07 08:27:08 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
-for let i = 0; i <botoes.length; (i++) {
+for (let i = 0; i <botoes.length; i++) {
+        botoes[i].onclick = function() {
+            
+        }
    console.log(i);
 }
 botoes[i],onclick = function () {
